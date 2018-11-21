16fa5f2c24ede67c737817f95648f3d229204f07
mrcallejas <m.jcacll@gmail.com>
2018-11-21 00:16:20 -0500

    Deteccion Nueva

--- Callejas/DC2.py
+++ Callejas/DC2.py
@@ -1,47 +1,38 @@
 import cv2
 import numpy
 
-def diferencia(MatrizA,MatrizB):
-    (TamAX,TamAY, TamAZ) = MatrizA.shape
-    for x in xrange(TamAX):
-        for y in xrange(TamAY):
-            (B1,G1,R1)=MatrizA[x,y]
-            (B2,G2,R2)=MatrizB[x,y]
+Imagen = cv2.imread("./image/LosV.jpg")
+Carlos = cv2.imread("./image/Carlos.jpg")
+Copito = cv2.imread("./image/Copito.jpg")
+Marcelo = cv2.imread("./image/Marcelo.jpg")
+Mario = cv2.imread("./image/Mario.jpg")
+Turco = cv2.imread("./image/Turco.jpg")
+Yimmi = cv2.imread("./image/Yimmi.jpg")
 
-            if R1!=R2 or G1!=G2 or B1!=B2:
-                print "Img1: ", (B1, G1, R1)
-                print "Img2: ", (B2, G2, R2)
-                print "Error en: ",(x,y)
-                return False
+def buscar(Matriz1,Matriz2):
+    (Ax, Ay, Az) = Matriz1.shape
+    (Bx, By, Bz) = Matriz2.shape
+    for x in xrange(Ax):
+        for y in xrange(Ay):
+            Muestra = Matriz1[x:(x + Bx), y:(y + By)]
+            (TamMX, TamMY, TamMZ) = Muestra.shape
 
-    return True
+            if TamMX == Bx and TamMY == By:
+                Diferencia = cv2.absdiff(Muestra, Matriz2)
+                if Diferencia.max() < 20:
+                    cv2.rectangle(Matriz1, (y, x), (y + By, x + Bx), (0, 0, 0), 1)
+                    cv2.imshow('Encontrado', Matriz1)
+                    cv2.waitKey(0)
 
-ImagenA = cv2.imread("./image/LosV.jpg")
-(Ax,Ay,Az) = ImagenA.shape
-ImagenB = cv2.imread("./image/Carlos.jpg")
-(Bx,By,Bz) = ImagenB.shape
-
-ImagenC= cv2.imread("./image/Carlos.jpg")
-(Cx,Cy,Cz) = ImagenB.shape
-diff=cv2.absdiff(ImagenB, ImagenC)
+buscar(Imagen,Carlos)
+buscar(Imagen,Copito)
+buscar(Imagen,Mario)
+buscar(Imagen,Marcelo)
+buscar(Imagen,Turco)
+buscar(Imagen,Yimmi)
 
 
-num=[0,0,0]
-BlackSection=numpy.full((Bx,By,Bz),num)
 
-for x in xrange(Bx):
-    for y in xrange(By):
-        Muestra=ImagenB[x:(x+10),y:(y+10)]
-        (TamMX,TamMY,TamMZ) = Muestra.shape
-        cv2.imshow('Diferencias detectadas', Muestra)
-        cv2.waitKey(0)
-"""
-        if TamMX == Bx and TamMY == By:
-            DiferenciaAB = cv2.absdiff(ImagenB, Muestra)
-            if diferencia(DiferenciaAB,diff)==True:
-                cv2.imshow('Diferencias detectadas', Muestra)
-                cv2.waitKey(0)
-"""
 
 
 
--- Callejas/Deteccion_caras.py
+++ Callejas/Deteccion_caras.py
@@ -4,7 +4,7 @@ import numpy as np
 Imagen = cv2.imread("./image/LosV.jpg")
 (cx,cy,cz)=Imagen.shape
 
-Carlos = cv2.imread()
+Carlos = cv2.imread("./image/Carlos.jpg")
 Copito = cv2.imread("./image/Copito.jpg")
 Marcelo = cv2.imread("./image/Marcelo.jpg")
 Mario = cv2.imread("./image/Mario.jpg")
@@ -15,6 +15,7 @@ def busqueda(section, name):
     (x,y,z)= section.shape
     respuesta = cv2.matchTemplate(Imagen, section, cv2.TM_CCOEFF_NORMED)
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(respuesta)
+    print max_val,min_val
     cv2.rectangle(Imagen, max_loc, (max_loc[0] + x - 5, max_loc[1] + y), (0, 0, 0), 1)
     cv2.imshow('umbral', Imagen)
     cv2.waitKey(0)
